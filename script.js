49af54ca16a6faac15bd450f140e5b10a0f700b7
Jack Stein <jacklsteiniv@gmail.com>
2015-11-30 12:22:30 -0800

    Refined shot logic, using Math.random and league averages per shot. Need to store player1/player2 id's as the player objects to be able to access their properties.

--- script.js
+++ script.js
@@ -27,14 +27,22 @@ $(function(){
 
   //instances of player object below with stats
   //                     dunk  long2 corner3 long3 defRating
-  var steph = new Player(0.68, 0.57, 0.45, 0.51, 0.80);
+  steph = new Player(0.68, 0.57, 0.45, 0.51, 0.80);
 
-  var lebron = new Player(0.72, 0.49, 0.50, 0.40, 0.95);
+  lebron = new Player(0.72, 0.49, 0.50, 0.40, 0.95);
 
-  var durant = new Player(0.65, 0.48, 0.45, 0.45, 0.80);
+  durant = new Player(0.65, 0.48, 0.45, 0.45, 0.80);
 
   //Kobe 12-13 numbers below, via Kirk Goldsberry shot charts
-  var kobe = new Player (0.60, 0.40, 0.32, 0.32, 0.60);
+  kobe = new Player (0.60, 0.40, 0.32, 0.32, 0.60);
+
+  //these are league averages for those shots, using
+  //mean of player group's shot averages /2 (accounting for Math.random)
+
+  dunkAvg = 0.33;
+  long2Avg = 0.24;
+  corner3Avg = 0.22;
+  long3Avg = 0.21;
 
   //Create jQuery click events for (1.) clicking on player,
   //storing as variable (2.) choosing shot,
@@ -44,6 +52,7 @@ $(function(){
   //Selecting your player, storing that variable
   $('#playerSelect1').click(function() {
     playerOne = $("#playersToChoose").val();
+    $(this).attr("disabled","disabled");
     //temporary alerts for testing
     alert("Player 1 chose " + playerOne);
     displayMessage("Player 2, your turn to pick a player");
@@ -53,80 +62,88 @@ $(function(){
 
   $('#playerSelect2').click(function() {
     playerTwo = $("#playersToChoose").val();
+    $(this).attr("disabled","disabled");
     alert("Player 2 chose " + playerTwo);
     $('#player2 h2').text(playerTwo);
+
     displayMessage("Game on!");
   });
 
 
   //Selecting your shot, running the probabilities, adding to count
+  //re-factor this.
 
   $('#shoot1').click(function() {
     //case #1: dunk. it's either 0 or 2
-    alert("Shots fired!");
     if($('#player1Shot').val() == 'dunk') {
       //calculate probability: Math.random(dunk), 0 or 2
-      //math.ceil rounds up, math.random is 0 or 1, dunk is dunk probab.
-      player1Points =  Math.ceil(Math.random() * this.dunk * 2);
-        if(player1Points > 0 ) {
-         alert("Player 1 scored " + player1Points + " points");
-         //display the div for a MAKE: append and show
-        $('#gifCentral').append('<img id="made" src="http://24.media.tumblr.com/bc5d571f93d62e8ca19aabca7bcff00b/tumblr_mf6j7pgcGV1qbjsl3o1_500.gif"/>');
-        $('#made').show();
+      // player1Points = Math.random * this.dunk;
+      //NOTE: dunk hard coded as 2 below, for testing purposes.
+      player1Points = 2;
+        if(player1Points > dunkAvg ) {
+          //if your percentage is greater than league avg, you make it
+          //and you get 2 points.
+          player1Points = 2;
+         //display the div for a MAKE: visible css
+        // $('#made').css('visibility', 'visible')
          //if it's 0 or 2, add it to the player1Count
          player1Count = player1Count + player1Points;
-
          //try returning your values so they can be accessed by alert
         }
         else {
             player1Points = 0;
             //display div for a MISS: append and show
-        $('#gifCentral').append('<img id = "missed" src = "http://cdn0.sbnation.com/assets/3931087/parkerft.gif"/>');
-        $('#missed').show();
+        // $('#missed').css('visibility', 'visible')
         }
-        alert("Player 1 scored " + player1Points + " points");
+        // alert("Player 1 scored " + player1Points + " points");
     //display the score in the div at bottom of player1
     $('#player1Score').text(player1Count);
-    changeTurn();
+    // changeTurn();
     }
     //case #2: long 2. it's either 0 or 2.
     if($('#player1Shot').val() == 'long2') {
-      player1Points = Math.ceil(Math.random() * this.long2 * 2);
-        if(player1Points >=2) {
+      player1Points = Math.random() * this.long2;
+        if(player1Points > long2Avg) {
+          player1Points = 2;
           player1Count += player1Points;
         }
         else {
           player1Points = 0;
         }
-    changeTurn();
+    // changeTurn();
     }
     //case #3: corner 3. it's either 0 or 3.
     if($('#player1Shot').val() == 'corner3') {
-      player1Points = Math.ceil(2*Math.random() * this.corner3 * 3);
-        if(player1Points >=3) {
+      player1Points = Math.random()* this.corner3;
+        if(player1Points > corner3Avg) {
           //if it's 3 or 0, add it to player1Count
+          player1Points = 3;
           player1Count += player1Points;
         }
         else {
           player1Points = 0;
         }
-    changeTurn();
+    // changeTurn();
     }
     //case #4: long 3. it's either 0 or 3.
     if($('#player1Shot').val() == 'long3') {
-      player1Points = Math.ceil(2*Math.random() * this.long3 * 3);
-        if(player1Points >=3) {
+      player1Points = Math.random() * this.long3;
+        if(player1Points > long3Avg) {
+          player1Points = 3;
           player1Count += player1Points;
         }
         else {
           player1Points = 0;
         }
-    changeTurn();
+    // changeTurn();
     }
     alert("Player 1 scored " + player1Points + " points");
     //display the score in the div at bottom of player1
     $('#player1Score').text(player1Count);
+    //hard-coded turn switch below
     changeTurn();
+    checkForWinner(player1Count, player2Count);
+    displayMessage("It's Player 2's turn to shoot!");
   });
 
 
@@ -134,18 +151,16 @@ $(function(){
 
 $('#shoot2').click(function() {
     //case #1: dunk. it's either 0 or 2
-    alert("Shots fired!");
     if($('#player2Shot').val() == 'dunk') {
+      player2Points = Math.random() * this.dunk;
       //calculate probability: Math.random(dunk), 0 or 2
-      //math.ceil rounds up, math.random is 0 or 1, dunk is dunk probab.
-      player2Points =  Math.ceil(Math.random() * this.dunk * 2);
-        if(player2Points > 0 ) {
-         alert("Player 2 scored " + player2Points + " points");
+        if(player2Points > dunkAvg ) {
          //display the div for a MAKE: append and show
-        $('#gifCentral').append('<img id="made" src="http://24.media.tumblr.com/bc5d571f93d62e8ca19aabca7bcff00b/tumblr_mf6j7pgcGV1qbjsl3o1_500.gif"/>');
-        $('#missed').hide();
-        $('#made').toggle();
+        // $('#gifCentral').append('<img id="made" src="http://24.media.tumblr.com/bc5d571f93d62e8ca19aabca7bcff00b/tumblr_mf6j7pgcGV1qbjsl3o1_500.gif"/>');
+        // $('#missed').hide();
+        // $('#made').toggle();
          //if it's 0 or 2, add it to the player1Count
+         player2Points = 2;
          player2Count = player2Count + player2Points;
 
          //try returning your values so they can be accessed by alert
@@ -153,53 +168,58 @@ $('#shoot2').click(function() {
         else {
             player2Points = 0;
             //display div for a MISS: append and show
-        $('#gifCentral').append('<img id = "missed" src = "http://cdn0.sbnation.com/assets/3931087/parkerft.gif"/>');
-        $('#missed').toggle();
-        $('#made').hide();
+        // $('#gifCentral').append('<img id = "missed" src = "http://cdn0.sbnation.com/assets/3931087/parkerft.gif"/>');
+        // $('#missed').toggle();
+        // $('#made').hide();
         }
-        alert("Player 2 scored " + player2Points + " points");
+
     //display the score in the div at bottom of player1
     $('#player2Score').text(player2Count);
-    changeTurn();
+    // changeTurn();
+
     }
     //case #2: long 2. it's either 0 or 2.
     if($('#player2Shot').val() == 'long2') {
-      player2Points = Math.ceil(Math.random() * this.long2 * 2);
-        if(player2Points >=2) {
+      player2Points = Math.random() * this.long2;
+        if(player2Points > long2Avg) {
+          player2Points = 2;
           player2Count += player2Points;
         }
         else {
           player2Points = 0;
         }
-    changeTurn();
+    // changeTurn();
     }
     //case #3: corner 3. it's either 0 or 3.
     if($('#player2Shot').val() == 'corner3') {
-      player2Points = Math.ceil(2*Math.random() * this.corner3 * 3);
-        if(player2Points >=3) {
+      player2Points = Math.random() * this.corner3;
+        if(player2Points > corner3Avg) {
           //if it's 3 or 0, add it to player1Count
           player2Count += player2Points;
         }
         else {
           player2Points = 0;
         }
-    changeTurn();
+    // changeTurn();
     }
     //case #4: long 3. it's either 0 or 3.
     if($('#player2Shot').val() == 'long3') {
-      player2Points = Math.ceil(2*Math.random() * this.long3 * 3);
-        if(player2Points >=3) {
+      player2Points = Math.random() * this.long3;
+        if(player2Points > long3Avg) {
           player2Count += player2Points;
         }
         else {
           player2Points = 0;
         }
-    changeTurn();
+    // changeTurn();
     }
     alert("Player 2 scored " + player2Points + " points");
     //display the score in the div at bottom of player1
     $('#player2Score').text(player2Count);
     changeTurn();
+    checkForWinner(player1Count, player2Count);
+    displayMessage("It's Player 1's turn to shoot!");
+
   });
 
 
@@ -223,31 +243,30 @@ $('#shoot2').click(function() {
 
   function changeTurn() {
     winresult = false;
-    // if(winresult = true) {
-    //   displayMessage("Congrats, " + turn + ", you won!");
-    //   winner = turn;
-    // }
+    if(winresult = true) {
+      displayMessage("Congrats, " + turn + ", you won!");
+      winner = turn;
+    }
     if(turn === "player1") {
-      turn = "player2";
+      turn === "player2";
       displayMessage("It's " + turn + "'s turn to shoot!");
     }
     else {
-      turn = "player1";
+      turn === "player1";
       displayMessage("It's " + turn + "'s turn to shoot!");
     }
-  }
+  };
 
 
   //Checking for a winner if count >=21,
   //this is called back in changeturn to see if a win has occurred.
 
   function checkForWinner(player1Count, player2Count) {
-    winresult = false;
-    if(player1Count || player2Count >= 21) {
+    if(player1Count || player2Count >= 6) {
       winresult = true;
       return true;
     }
-  }
+  };
 
 
 });
